1f1f74970ea3d8d9367ad627833607c7dc50f197
Johannes Hupfeld <j.hupfeld@gmail.com>
2019-10-20 02:58:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
-lass Dog
+class Dog
+  # Class body
+ 
+  # Instance Method Definition
   def bark
     puts "Woof!"
   end
 end
  
 fido = Dog.new
-fido.bark #> "Woof!"
- 
-snoopy = Dog.new
-snoopy.bark
+fido.bark
